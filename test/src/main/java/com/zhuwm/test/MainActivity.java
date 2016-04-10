7a795f158da5eba83f5aea382eced198d828db9d
zhuwm <zhuwm@cairenhui.com>
2016-04-10 22:40:41 +0800

    commit

--- test/src/main/java/com/zhuwm/test/MainActivity.java
+++ test/src/main/java/com/zhuwm/test/MainActivity.java
@@ -2,13 +2,41 @@ package com.zhuwm.test;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.ViewGroup;
 
 public class MainActivity extends AppCompatActivity {
+
+    private RecyclerView rv;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
 
+        rv = new RecyclerView(this);
+
+        setContentView(rv);
+
+        rv.setLayoutManager(new LinearLayoutManager(this));
+
+        rv.setAdapter(new RecyclerView.Adapter() {
+            @Override
+            public RecyclerView.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+
+                return null;
+            }
+
+            @Override
+            public void onBindViewHolder(RecyclerView.ViewHolder holder, int position) {
+
+            }
+
+            @Override
+            public int getItemCount() {
+                return 0;
+            }
+        });
     }
 }
