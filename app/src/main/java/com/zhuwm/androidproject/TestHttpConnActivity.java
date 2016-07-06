29ac9cb9b87e8972df864d1fea7693b2fce4c634
zhuwm <zhuwm@cairenhui.com>
2016-07-06 18:10:54 +0800

    json

--- app/src/main/java/com/zhuwm/androidproject/TestHttpConnActivity.java
+++ app/src/main/java/com/zhuwm/androidproject/TestHttpConnActivity.java
@@ -10,8 +10,13 @@ import android.widget.TextView;
 import com.android.volley.RequestQueue;
 import com.android.volley.Response;
 import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonArrayRequest;
+import com.android.volley.toolbox.JsonObjectRequest;
 import com.android.volley.toolbox.StringRequest;
 import com.android.volley.toolbox.Volley;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
 
 public class TestHttpConnActivity extends Activity {
     private static String TAG="TestHttpConnActivity";
@@ -25,9 +30,32 @@ public class TestHttpConnActivity extends Activity {
 
         findViewById(R.id.btnConnectServer).setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
-                connServer();
+                //connServer();
+                connServerUserJson();
+
+            }
+
+
+        });
+    }
+
+    private void connServerUserJson() {
+        tvResult = (TextView) findViewById(R.id.tvHttpResult);
+
+        JsonArrayRequest jsonObjectRequest = new JsonArrayRequest("http://192.168.3.72:8080/h5test/json.do",
+                new Response.Listener<JSONArray>() {
+                    @Override
+                    public void onResponse(JSONArray response) {
+                        Log.d("TAG", response.toString());
+                        tvResult.setText(response.toString());
+                    }
+                }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Log.e("TAG", error.getMessage(), error);
             }
         });
+        Volley.newRequestQueue(this).add(jsonObjectRequest);
     }
 
     private void connServer() {
